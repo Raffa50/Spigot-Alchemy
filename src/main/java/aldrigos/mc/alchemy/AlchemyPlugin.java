83617641c56ef67f35c8f211ef3f872743694ddb
Raffaele Aldrigo <r.aldrigo@reply.it>
2020-01-10 17:08:45 +0100

    potion fusion works

--- src/main/java/aldrigos/mc/alchemy/AlchemyPlugin.java
+++ src/main/java/aldrigos/mc/alchemy/AlchemyPlugin.java
@@ -1,11 +1,26 @@
 package aldrigos.mc.alchemy;
 
 import aldrigos.mc.alchemy.listeners.*;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlchemyPlugin extends JavaPlugin {
+    private Recipe getFusePotionRecipe(){
+        var r = new ShapelessRecipe(
+            new NamespacedKey(this, "fusedpotion"),
+            new ItemStack(Material.POTION)
+        );
+        r.addIngredient(2, Material.POTION);
+
+        return r;
+    }
+
     @Override
     public void onEnable(){
+        getServer().addRecipe(getFusePotionRecipe());
+
         getServer().getPluginManager().registerEvents(new CraftListener(this), this);
     }
 }
--- src/main/java/aldrigos/mc/alchemy/listeners/CraftListener.java
+++ src/main/java/aldrigos/mc/alchemy/listeners/CraftListener.java
@@ -16,19 +16,34 @@ public class CraftListener implements Listener {
         p = plugin;
     }
 
-    @EventHandler
-    public void onCraft(CraftItemEvent e){
-        var res = new ItemStack(Material.POTION);
-        var meta =  (PotionMeta) Bukkit.getItemFactory().getItemMeta(Material.POTION);
+    private ItemStack potionFusion(ItemStack[] craftMatrix){
+        var meta = (PotionMeta) Bukkit.getItemFactory().getItemMeta(Material.POTION);
 
-        for(var ing: e.getInventory().getMatrix()) {
-            p.getLogger().info("[Alchemy]"+ing.getType().toString());
-            if (ing.getType() != Material.POTION)
-                return;
+        for(var ing: craftMatrix) {
+            if(ing == null)
+                continue;
+            if (ing.getType() != Material.POTION || !ing.hasItemMeta())
+                return null;
+
+            var fuse = (PotionMeta) ing.getItemMeta();
+            var pd = fuse.getBasePotionData();
+            if(pd.getType().getEffectType() != null)
+                meta.addCustomEffect(new PotionEffect(pd.getType().getEffectType(), 20*60, 1), true);
+
+            for(var ce: fuse.getCustomEffects())
+                meta.addCustomEffect(ce, true);
         }
 
-        var data = new PotionData(PotionType.THICK);
-        meta.setBasePotionData(data);
+        meta.setBasePotionData(new PotionData(PotionType.THICK));
+        var res = new ItemStack(Material.POTION);
         res.setItemMeta(meta);
+        return res;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onCraft(PrepareItemCraftEvent e){
+        ItemStack res = potionFusion(e.getInventory().getMatrix());
+        if(res != null)
+            e.getInventory().setResult(res);
     }
 }
