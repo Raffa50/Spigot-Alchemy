668af162a71dc568788ef7cfb9dd5e7f10e3b5cc
Raffaele Aldrigo <r.aldrigo@reply.it>
2020-01-15 15:05:30 +0100

    consider if the potion is upgraded / extended

--- src/main/java/aldrigos/mc/alchemy/Alchemy.java
+++ src/main/java/aldrigos/mc/alchemy/Alchemy.java
@@ -6,6 +6,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 public class Alchemy {
     public static ItemStack createFusedPotion(Iterable<ItemStack> potions){
         var meta = (PotionMeta) Bukkit.getItemFactory().getItemMeta(Material.POTION);
@@ -14,8 +18,12 @@ public class Alchemy {
             var fuse = (PotionMeta) p.getItemMeta();
             var pd = fuse.getBasePotionData();
             var eff = pd.getType().getEffectType();
+            int duration = pd.isExtended() ? 20*180 : 20*90;
+
             if( eff != null)
-                meta.addCustomEffect(new PotionEffect(eff, 20*90, 0), true);
+                meta.addCustomEffect(
+                    new PotionEffect(eff, duration, pd.isUpgraded() ? 1 : 0),
+                true);
 
             for(var ce: fuse.getCustomEffects())
                 meta.addCustomEffect(ce, true);
@@ -25,5 +33,15 @@ public class Alchemy {
         var res = new ItemStack(Material.POTION);
         res.setItemMeta(meta);
         return res;
+    }
+
+    private static final Map<PotionType, Integer> potionDuration = new HashMap<>();
+
+    static {
+
+    }
+
+    public static int getDuration(PotionType pt, boolean extended, boolean enhanced){
+        return 20*60;
     }
 }
