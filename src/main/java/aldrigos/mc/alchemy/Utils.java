2582d0aedd5b295bc5ab6a1cf8943e54bb5ec16a
Raffaele Aldrigo <r.aldrigo@reply.it>
2020-01-22 14:47:15 +0100

    WIP since first aproach didn't worked, have to use a custom background task...

--- src/main/java/aldrigos/mc/alchemy/Utils.java
+++ src/main/java/aldrigos/mc/alchemy/Utils.java
@@ -1,10 +1,16 @@
 package aldrigos.mc.alchemy;
 
-import java.lang.reflect.Array;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 import java.util.List;
 
 public abstract class Utils {
     public static <T> T[] toArray(List<T> list){
         return (T[])list.toArray();
+    }
+
+    public static boolean isEffectPotion(ItemStack i){
+        return i != null && i.getType() == Material.POTION && i.hasItemMeta();
     }
 }
--- src/main/java/aldrigos/mc/alchemy/listeners/BrewListener.java
+++ src/main/java/aldrigos/mc/alchemy/listeners/BrewListener.java
@@ -3,27 +3,53 @@ package aldrigos.mc.alchemy.listeners;
 import aldrigos.mc.alchemy.*;
 import org.bukkit.Material;
 import org.bukkit.event.*;
-import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class BrewListener implements Listener {
     private final AlchemyPlugin plugin;
+    private final Logger log;
 
     public BrewListener(AlchemyPlugin plugin){
         this.plugin = plugin;
+        log = plugin.getLogger();
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBrewInventory(InventoryClickEvent e){
+        if(e.getInventory().getType() != InventoryType.BREWING || e.getAction() != InventoryAction.PLACE_ALL)
+            return;
+
+        var brewInv = (BrewerInventory) e.getInventory();
+        var ingr = e.getCursor();
+
+        //log.info(" brewInventory: "+brewInv.toString());
+
+        if(ingr != null && ingr.getType() == Material.GOLD_NUGGET &&
+            Arrays.stream(brewInv.getStorageContents()).anyMatch(Utils::isEffectPotion)
+        ) {
+            e.getWhoClicked().getInventory().remove(ingr);
+            //TODO
+            //brewInv.setIngredient(ingr); //doesn't work
+            //brewInv.getHolder().setBrewingTime(20 * 20);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBrew(BrewEvent e){
         var ingredient = e.getContents().getIngredient();
         if(ingredient == null)
             return;
 
         switch (ingredient.getType()) {
-            case REDSTONE:
-            case GLOWSTONE:
-                handleExtendedUpgrade(e);
+            case GOLD_NUGGET:
+                log.info("handleExtendedUpgrade");
+                handleExtendedUpgrade(e.getContents());
                 break;
             case GUNPOWDER:
                 handleSplash(e);
@@ -34,10 +60,10 @@ public class BrewListener implements Listener {
         }
     }
 
-    private void handleExtendedUpgrade(BrewEvent e) {
+    private void handleExtendedUpgrade(BrewerInventory inv) {
         for(int i=0; i< 3; i++){
-            ItemStack result = e.getContents().getItem(i);
-            if(result == null || !result.hasItemMeta() || result.getType() != Material.POTION)
+            ItemStack result = inv.getItem(i);
+            if(!Utils.isEffectPotion(result))
                 continue;
 
             var pot = (PotionMeta) result.getItemMeta();
@@ -48,7 +74,7 @@ public class BrewListener implements Listener {
                         Alchemy.createExtendedUpgradedPotion(Utils.toArray(pot.getCustomEffects())) :
                         Alchemy.createExtendedUpgradedPotion(pd.getType());
 
-                e.getContents().setItem(i, eup);
+                inv.setItem(i, eup);
             }
         }
     }
@@ -73,7 +99,7 @@ public class BrewListener implements Listener {
     private void handleSplash(BrewEvent e) {
         for(int i=0; i< 3; i++) {
             ItemStack result = e.getContents().getItem(i);
-            if(result == null || !result.hasItemMeta() || result.getType() != Material.POTION)
+            if(!Utils.isEffectPotion(result))
                 continue;
 
             var pot = (PotionMeta) result.getItemMeta();
